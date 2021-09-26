584b11c183f36c91564363d4f87390e06c38d481
Sourav Roy <roys90752@gmail.com>
2021-09-26 21:04:38 +0530

    "notification Intent
    "

--- app/src/main/java/com/nullediots/empty/MainActivity.kt
+++ app/src/main/java/com/nullediots/empty/MainActivity.kt
@@ -2,11 +2,14 @@ package com.nullediots.empty
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 
 import android.content.pm.PackageManager
-import android.os.Build
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -19,10 +22,16 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.TaskStackBuilder
 
 class MainActivity : AppCompatActivity() {
 
-
+    private val  CHANNEL_ID = "channelId"
+    private val CHANNEL_NAME = "channelName"
+        private val NOTIFICATION_ID = 0
 
     @SuppressLint("CommitPrefEdits")
     @RequiresApi(Build.VERSION_CODES.Q)
@@ -39,6 +48,7 @@ class MainActivity : AppCompatActivity() {
         val btnDialogActivity = findViewById<Button>(R.id.btn_dialog)
         val btnSave = findViewById<Button>(R.id.btnSave)
         val btnLoad = findViewById<Button>(R.id.btnPref)
+        val btnNotification = findViewById<Button>(R.id.btnNotification)
         val etName = findViewById<EditText>(R.id.etName)
 
         btnSave.setOnClickListener {
@@ -73,7 +83,30 @@ class MainActivity : AppCompatActivity() {
                 startActivity(it)
             }
             }
+
+        /////////////////////////////////////Notification/////////////////////////////////
+
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
         }
+
+        createNotificationChannel()
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.id.search_mag_icon)
+            .setContentTitle("Superman is Best")
+            .setContentText("CSK will win IPl")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        btnNotification.setOnClickListener {
+            notificationManager.notify(NOTIFICATION_ID, notification)
+        }
+    }
 
     private fun checkStoragePermission() = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
 
@@ -120,6 +153,9 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+
+
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar, menu)
         return true
@@ -133,5 +169,17 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+
+    private fun createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val notificationChannel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT).apply {
+                    lightColor = Color.GREEN
+                    enableLights(true)
+            }
+            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(notificationChannel)
+        }
+    }
 
 }
